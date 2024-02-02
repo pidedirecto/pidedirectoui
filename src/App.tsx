79bc91035bbadccfe5300d57a85df177c809b4c9
Oscar Moreno <oscar@pidedirecto.mx>
2024-02-01 18:26:13 -0600

    improve vite config

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
  * @prettier
  */
 import * as React from 'react';
+import { Checkbox } from "src/components";
 
 export function App(): React.ReactElement {
     return (
         <div>
             <h1>Checkbox</h1>
+            <Checkbox label={'Checkbox'} name={'checkbox'} />
         </div>
     );
 }
