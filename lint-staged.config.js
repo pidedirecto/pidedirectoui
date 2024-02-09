35d11b513d41ee0bc8c3511cc819f5c7cf1c4d94
Oscar Moreno <oscar@pidedirecto.mx>
2024-02-09 16:38:32 -0600

    remove type module from package.json

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,5 +4,5 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}':
         'eslint --quiet --fix --rule "unused-imports/no-unused-imports: error" --rule "no-relative-import-paths/no-relative-import-paths: error" --format "node_modules/eslint-friendly-formatter"',
-    '**/*': 'prettier --write --config .prettierrc.cjs --ignore-unknown',
+    '**/*': 'prettier --write --config .prettierrc.js --ignore-unknown',
 };
