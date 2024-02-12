d563a1c625212126d8a2d1f3e1bb4a4802813584
Oscar Moreno <oscar@pidedirecto.mx>
2024-02-12 12:45:06 -0600

    remove dist imports in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 /**
  * @prettier
  */
-import { Checkbox } from 'dist';
-import { QuestionIcon } from 'dist/icons';
+// import { Checkbox } from 'dist';
+// import { QuestionIcon } from 'dist/icons';
 import * as React from 'react';
 
 export function App(): React.ReactElement {
     return (
         <div>
             <h1>Checkbox</h1>
-            <QuestionIcon />
-            <Checkbox name={'name'} label={'New checkbox'} />
+            {/*<QuestionIcon />*/}
+            {/*<Checkbox name={'name'} label={'New checkbox'} />*/}
         </div>
     );
 }
