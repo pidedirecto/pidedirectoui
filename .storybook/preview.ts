d50f8daed815a334c68e65eb1189fbfee77f6ddd
Oscar Moreno <oscar@pidedirecto.mx>
2024-02-14 12:33:58 -0600

    format preview file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,15 +3,15 @@ import 'src/letseatfont.css';
 import 'src/index.css';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
