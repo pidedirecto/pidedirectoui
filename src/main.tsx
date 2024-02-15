ce36efb61efaa663904680adbfbf27715f3f7eab
Oscar Moreno <oscar@pidedirecto.mx>
2024-02-15 17:20:20 -0600

    remove Provider from main component

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,13 @@
  */
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 // eslint-disable-next-line import/named
 import { App } from 'src/App';
-import { store } from 'src/config/store';
 import 'src/index.css';
 import 'src/letseatfont.css';
 
 function Main(): React.ReactElement {
-    return (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
+    return <App />;
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
