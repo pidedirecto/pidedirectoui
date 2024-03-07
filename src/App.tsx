5537d48799e4f80a6c876a56e88548071a172cd5
SoyTanMax <soytanmax10@gmail.com>
2024-03-07 16:15:05 -0600

    changed button styles to use new tokens

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,7 @@
  */
 
 import * as React from 'react';
-import { useForm } from 'react-hook-form';
-import { Switch } from 'src/components/Switch';
-import { Form } from 'src/form/Form';
-import { FormSwitch } from 'src/form/FormSwitch';
 
 export function App(): React.ReactElement {
-    const form = useForm();
-    return (
-        <div>
-            <Switch name={'asdas1'} label={'asdas'} />
-            <hr />
-            <Switch name={'asdas2'} label={'asdas'} checked={true} value={true} />
-            <hr />
-            <Switch name={'asdas3'} label={'asdas'} disabled />
-            <hr />
-            <Switch name={'asdas4'} label={'asdas'} checked={true} disabled />
-            <hr />
-            <Switch name={'asdas5'} label={'asdas'} error />
-            <hr />
-            <Form form={form} onSubmit={console.log}>
-                <FormSwitch name={'asda'} label={'form switch'} />
-                <button type={'submit'}>submit</button>
-            </Form>
-        </div>
-    );
+    return <div></div>;
 }
