0c792124970f55485d054a6a10262a782a9afeb9
Oscar Moreno <oscar@pidedirecto.mx>
2024-03-08 16:06:08 -0600

    add UseIsScreenSize.d.ts

--- src/hooks/index.d.ts
+++ src/hooks/index.d.ts
@@ -1,6 +1,7 @@
 import { UseTheme } from "src/types/hooks/UseTheme";
 import { UseForm } from "src/types/hooks/UseForm";
 import { UseHasClickedOutside } from "src/types/hooks/UseHasClickedOutside";
+import { UseIsScreenSize } from "src/types/hooks/UseIsScreenSize";
 
 /**
  * Theme hook to override default components theme
@@ -22,3 +23,10 @@ export const useForm: UseForm
  * [docs](https://ui.pidedirecto.mx/?path=/docs/hooks-usehasclickedoutside--docs)
  */
 export const useHasClickedOutside: UseHasClickedOutside
+
+/**
+ * Hook to known if the current screen width is of a specific size
+ *
+ * [docs](https://ui.pidedirecto.mx/?path=/docs/hooks-useisscreensize--docs)
+ */
+export const useIsScreenSize: UseIsScreenSize
