4b7dc6828e896163a9656d7f412a23a12142531d
Hector Wong <hector.wong@pidedirecto.mx>
2024-04-08 09:25:40 -0700

    add MultiselectableAutocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,35 @@
  */
 
 import * as React from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { DropDown } from 'src/components/DropDown';
 import { DropDownItem } from 'src/components/DropDownItem';
-import { VirtualizedList } from 'src/components/VirtualizedList';
+import { HelperText } from 'src/components/HelperText';
+import { MultiselectableAutocomplete } from 'src/components/MultiselectableAutocomplete';
 import { FoodBoxIcon } from 'src/icons/FoodBoxIcon';
 import { RestoreIcon } from 'src/icons/RestoreIcon';
 
 export function App(): React.ReactElement {
     const form = useForm();
 
-    const itemsFiltered = [{ value: 'pepe' }, { value: 'pepo' }, { value: 'pepa' }, { value: 'papa' }, { value: 'pap1' }, { value: 'pap2' }, { value: 'papa3' }, { value: 'papa4' }, { value: 'pap5' }];
+    const [itemsSelected, setItemsSelected] = useState([]);
 
+    const itemsFiltered = [
+        { value: 'pepe', label: 'proando 1' },
+        { value: 'pepo', label: 'proando 2' },
+        { value: 'pepa', label: 'proando 3' },
+        { value: 'papa', label: 'proando 4' },
+        { value: 'pap1', label: 'proando 5' },
+        { value: 'pap2', label: 'proando 6' },
+        { value: 'papa3', label: 'proando 7' },
+        { value: 'papa4', label: 'proando 8' },
+        { value: 'pap5', label: 'proando 9' },
+    ];
+
+    const handleChange = (itemIds: any) => {
+        console.log({ itemIds });
+    };
     return (
         <div>
             <RestoreIcon />
@@ -32,11 +49,30 @@ export function App(): React.ReactElement {
                 <DropDownItem>sda</DropDownItem>
                 <DropDownItem>asdas</DropDownItem>
             </DropDown>
-            <VirtualizedList height={120} itemSize={20}>
-                {itemsFiltered.map((item, idx) => (
-                    <div key={item.value}>{item.value}</div>
-                ))}
-            </VirtualizedList>
+            <MultiselectableAutocomplete
+                name={'example'}
+                selectAllOptionLabel={'seleccionar todos'}
+                productsSelectedLabel={'productos seleccionados'}
+                data={itemsFiltered}
+                getOptionValue={(option) => option.value}
+                getOptionLabel={(option) => option.label}
+                renderOption={(option) => (
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            padding: '8px 4px',
+                        }}
+                    >
+                        <p>{option.menuItemName}</p>
+                        <HelperText>({option?.restaurantName})</HelperText>
+                    </div>
+                )}
+                onChange={(itemIds: any) => handleChange(itemIds)}
+                selectedItems={itemsSelected}
+            />
         </div>
     );
 }
---
+++ src/components/MultiselectableAutocomplete.tsx
@@ -0,0 +1,160 @@
+/**
+ * @prettier
+ */
+import { useAutocomplete } from '@mui/base/useAutocomplete';
+import { useRef, useState } from 'react';
+import * as React from 'react';
+import { createPortal } from 'react-dom';
+import { Checkbox } from 'src/components/Checkbox';
+import { HelperText } from 'src/components/HelperText';
+import { Input } from 'src/components/Input';
+import { Label } from 'src/components/Label';
+import classes from 'src/styles/multiselectableAutocomplete.module.css';
+import { MultiselectableAutocompleteProps } from 'src/types/components/MultiselectableAutoComplete';
+import { classNames } from 'src/utils/css/classNames';
+import { debounce } from 'src/utils/function/debounce';
+
+export function MultiselectableAutocomplete({
+    label,
+    name,
+    placeholder,
+    helperText,
+    data,
+    renderOption,
+    getOptionDisabled,
+    getOptionLabel,
+    getOptionValue,
+    onChange,
+    disabled,
+    error,
+    selectAllOption,
+    classes: classesProp,
+    selectedItems,
+    productsSelectedLabel,
+    selectAllOptionLabel,
+}: MultiselectableAutocompleteProps): React.ReactElement {
+    const listboxContainerRef = useRef<HTMLDivElement | null>(null);
+
+    const [inputValue, setInputValue] = useState('');
+    const [showOptions, setShowOptions] = useState(false);
+
+    const { getRootProps, getInputProps, getListboxProps, getOptionProps, groupedOptions } = useAutocomplete({
+        id: name,
+        options: data,
+        getOptionLabel,
+        getOptionDisabled,
+    });
+
+    const handleItem = (itemId: string) => {
+        if (getOptionDisabled?.(itemId)) return;
+        if (selectedItems.includes(itemId)) {
+            const updateSelectedItems = selectedItems.filter((item) => item !== itemId);
+            onChange([...updateSelectedItems]);
+            return;
+        }
+        onChange([...selectedItems, itemId]);
+    };
+
+    const handleSelectAllItem = () => {
+        const itemsId = data.map((itemId) => getOptionValue(itemId));
+        if (selectedItems.length === data.length) {
+            onChange([]);
+            return;
+        }
+        onChange(itemsId);
+    };
+
+    const getListboxTopPosition = () => {
+        if (!listboxContainerRef.current) return 0;
+
+        const clientRect = listboxContainerRef.current.getBoundingClientRect();
+        return clientRect.top + 10;
+    };
+
+    const getListboxLeftPosition = () => {
+        if (!listboxContainerRef.current) return 0;
+
+        const clientRect = listboxContainerRef.current.getBoundingClientRect();
+        return clientRect.left;
+    };
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        getInputProps().onChange?.(e);
+    };
+
+    const handleOnBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        getInputProps().onBlur?.(e);
+    };
+
+    const handleOnFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+        getInputProps().onFocus?.(e);
+    };
+
+    const debouncedOnChange = debounce(handleOnChange, 700);
+
+    return (
+        <div>
+            <div {...getRootProps()}>
+                <div className={classes.headContainer}>
+                    <Label htmlFor={`use-autocomplete-customer`} classes={{ label: classes.label, error: classes.labelError }} error={!!error}>
+                        {label}
+                    </Label>
+                    {!showOptions && <span className={classes.numberItemsSelectedContainer}>{productsSelectedLabel}</span>}
+                </div>
+                <Input
+                    {...getInputProps()}
+                    name={`use-autocomplete-customer`}
+                    type='search'
+                    classes={{ input: classes.input, label: classes.inputError }}
+                    value={inputValue}
+                    onChange={(value, e) => {
+                        setInputValue(value);
+                        debouncedOnChange(e);
+                    }}
+                    onBlur={(e) => {
+                        setShowOptions(false);
+                        handleOnBlur(e);
+                    }}
+                    onFocus={(e) => {
+                        setShowOptions(true);
+                        handleOnFocus(e);
+                    }}
+                    placeholder={placeholder}
+                    disabled={disabled}
+                />
+                {!!helperText && <HelperText classes={{ helperText: classes.helperText }}>{helperText}</HelperText>}
+            </div>
+            <div ref={listboxContainerRef} style={{ width: '100%' }}>
+                {groupedOptions.length > 0 &&
+                    showOptions &&
+                    createPortal(
+                        <ul
+                            className={classNames(classes.listbox, classesProp?.optionsContainer)}
+                            {...getListboxProps()}
+                            style={{ top: getListboxTopPosition(), left: getListboxLeftPosition(), width: listboxContainerRef.current?.offsetWidth }}
+                        >
+                            {selectAllOption && (
+                                <li onClick={() => handleSelectAllItem()} className={classNames(classesProp?.optionContainer, classes.checkAllBoxRow)}>
+                                    <Checkbox name={'selectAll'} label={selectAllOptionLabel} value={'all'} checked={selectedItems.length === data.length} onChange={() => handleSelectAllItem()} />
+                                    <span className={classes.numberItemsSelectedContainer}>{productsSelectedLabel}</span>
+                                </li>
+                            )}
+                            {groupedOptions.map((option: any, index: number) => (
+                                <>
+                                    <li
+                                        {...getOptionProps({ option, index })}
+                                        onClick={() => handleItem(getOptionValue(option))}
+                                        className={classNames(classes.checkBoxRow, classesProp?.optionContainer)}
+                                    >
+                                        <Checkbox name={option.value} value={option.value || undefined} checked={selectedItems?.includes(getOptionValue(option))} onChange={() => handleItem(option)} />
+                                        {renderOption(option)}
+                                    </li>
+                                </>
+                            ))}
+                        </ul>,
+                        document.body,
+                    )}
+            </div>
+        </div>
+    );
+}
