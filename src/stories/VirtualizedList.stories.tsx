ad1ce20f1ae500c1d1d3592f184c857471c07f30
Violeta Vera <violeta.vera@pidedirecto.mx>
2024-04-26 10:23:09 -0600

    add horizontal variant in VirtualizedList

--- src/stories/VirtualizedList.stories.tsx
+++ src/stories/VirtualizedList.stories.tsx
@@ -17,6 +17,13 @@ const meta: Meta<typeof VirtualizedList> = {
                 name: 'number',
             },
         },
+        width: {
+            description: 'Width to show List content',
+            type: {
+                required: false,
+                name: 'number',
+            },
+        },
         itemSize: {
             description: 'Size for each List Item',
             defaultValue: 150,
@@ -27,6 +34,13 @@ const meta: Meta<typeof VirtualizedList> = {
         },
         overScanCount: {
             description: 'This allows you to define how many elements outside the visible "window" will be rendered at all times.',
+        },
+        orientation: {
+            description: 'This property is assigned to the layout and allows to have a horizontal list, by default it is vertical.',
+            type: {
+                required: false,
+                name: 'string',
+            },
         },
         children: {
             description: 'List content',
@@ -47,6 +61,30 @@ export const Controlled: Story = {
     },
     render: () => (
         <VirtualizedList>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+            <div>Item 5</div>
+            <div>Item 6</div>
+            <div>Item 7</div>
+            <div>Item 8</div>
+            <div>Item 9</div>
+            <div>Item 10</div>
+        </VirtualizedList>
+    ),
+};
+
+export const Horizontal: Story = {
+    args: {
+        ...meta.args,
+        orientation: 'horizontal',
+        width: 800,
+        height: 60,
+        itemSize: 50,
+    },
+    render: () => (
+        <VirtualizedList orientation={'horizontal'} width={800} height={60} itemSize={150}>
             <div>Item 1</div>
             <div>Item 2</div>
             <div>Item 3</div>
--- src/types/components/VirtualizedList.d.ts
+++ src/types/components/VirtualizedList.d.ts
@@ -2,8 +2,10 @@ import * as React from "react";
 
 export type VirtualizedListProps = {
     height?: number,
+    width?: number,
     itemSize?:number,
     overScanCount?:number,
+    orientation?: 'horizontal',
     children: React.ReactNode,
 
 };
