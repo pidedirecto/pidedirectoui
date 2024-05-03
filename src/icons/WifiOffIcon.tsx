c8ec7fa5819f91b2132f7e6bd7bf3ce919feb2d3
Oscar Moreno <oscar@pidedirecto.mx>
2024-05-03 15:02:53 -0600

    fix title in WifiOffIcon

--- src/icons/WifiOffIcon.tsx
+++ src/icons/WifiOffIcon.tsx
@@ -17,7 +17,7 @@ export function WifiOffIcon({ color, title, width, height, size, ...props }: Svg
             strokeLinecap='round'
             strokeLinejoin='round'
         >
-            <title>title</title>
+            <title>{title}</title>
             <path stroke='none' d='M0 0h24v24H0z' fill='none' />
             <path d='M12 18l.01 0' />
             <path d='M9.172 15.172a4 4 0 0 1 5.656 0' />
