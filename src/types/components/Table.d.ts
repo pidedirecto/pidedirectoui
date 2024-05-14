7e67549cb09df5372877b3dd4809919bca7fd924
Violeta Vera <violeta.vera@pidedirecto.mx>
2024-05-14 15:39:21 -0600

    add key to rows

--- src/types/components/Table.d.ts
+++ src/types/components/Table.d.ts
@@ -30,6 +30,7 @@ export type TableColumn = {
 
 export type TableRow = Omit<Record<string, React.ReactNode>, 'className', 'onSearch'> & {
     className?: string,
+    key: string,
     onSearch?: (query: string) => boolean,
 }
 
