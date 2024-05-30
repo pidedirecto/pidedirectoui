7fd36916e47e2e5fa17d201150498c0af760783d
Hector Wong <hector.wong@pidedirecto.mx>
2024-05-30 07:57:39 -0600

    fix card

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { SaveIcon } from 'src/icons/SaveIcon';
 export function App(): React.ReactElement {
     return (
         <div style={{ padding: 120 }}>
-            <Card title={'titulo'}>
+            <Card>
                 <CircleContainer>
                     <SaveIcon />
                 </CircleContainer>
