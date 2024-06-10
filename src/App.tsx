d5791f53ebf5f75e8b192036db4f2b37087605a8
Hector Wong <hector.wong@pidedirecto.mx>
2024-06-10 14:20:29 -0600

    remove app tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,13 @@
  */
 
 import * as React from 'react';
-import { Button } from 'src/components/Button';
 import { Card } from 'src/components/Card';
 import { CircleIconContainer } from 'src/components/CircleIconContainer';
-import { ConfirmDialog } from 'src/components/confirmDialog/ConfirmDialog';
-import { useConfirmDialog } from 'src/hooks/useConfirmDialog';
 import { SaveIcon } from 'src/icons/SaveIcon';
 
 export function App(): React.ReactElement {
-    const confirmDialog = useConfirmDialog();
-
-    const handleClick = async () => {
-        const confirm = await confirmDialog({ title: 'pepe' });
-        console.log({ confirm });
-    };
-
     return (
         <div style={{ padding: 120 }}>
-            <ConfirmDialog />
-            <Button onClick={handleClick}>pepe</Button>
             <Card>
                 <CircleIconContainer>
                     <SaveIcon />
