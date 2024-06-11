4f6d3be57c41cf8028ef294a51a182c9525d7fe5
Oscar Moreno <oscar@pidedirecto.mx>
2024-06-11 13:52:36 -0600

    fix size prop in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ import { ButtonProps } from 'src/types/components/Button';
 import { classNames } from 'src/utils/css/classNames';
 import { convertReactNodeToString } from 'src/utils/react/convertReactNodeToString';
 
-export function Button({ classes: classesProp, type, children, onClick, variant, ...props }: ButtonProps): React.ReactElement {
+export function Button({ classes: classesProp, type, children, onClick, variant, size, ...props }: ButtonProps): React.ReactElement {
     const createUserClickedButtonLogEvent = useCreateUserClickedButtonLogEvent();
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -28,7 +28,7 @@ export function Button({ classes: classesProp, type, children, onClick, variant,
     };
 
     return (
-        <button {...props} data-variant={variant} type={type ?? 'button'} onClick={handleClick} className={getClassName()}>
+        <button {...props} data-size={size} data-variant={variant} type={type ?? 'button'} onClick={handleClick} className={getClassName()}>
             {children}
         </button>
     );
