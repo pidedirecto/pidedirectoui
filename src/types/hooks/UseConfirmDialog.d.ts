c6a3cd7500ce0ac6bcb08e397a3b2bf2864bbd1c
Hector Wong <hector.wong@pidedirecto.mx>
2024-06-13 10:25:59 -0600

    refactor UseConfirmDialogProps

--- src/types/hooks/UseConfirmDialog.d.ts
+++ src/types/hooks/UseConfirmDialog.d.ts
@@ -5,8 +5,6 @@
 export type UseConfirmDialog = () => (params: UseConfirmDialogProps) => Promise<boolean>;
 
 export type UseConfirmDialogProps = {
-    onAccept: (() => void) | undefined;
-    onCancel: (() => void) | undefined;
     title?: string;
     content?: string;
     acceptButtonText?: string;
