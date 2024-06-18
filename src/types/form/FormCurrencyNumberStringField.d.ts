6c5b322545232285f542f37cfa4c51b1e2f4848f
Oscar Moreno <oscar@pidedirecto.mx>
2024-06-18 14:53:01 -0600

    add country prop to FormCurrencyNumberStringField + utils

--- src/types/form/FormCurrencyNumberStringField.d.ts
+++ src/types/form/FormCurrencyNumberStringField.d.ts
@@ -3,6 +3,7 @@
  */
 import * as React from 'react';
 import { type ValidationRule } from 'react-hook-form';
+import { CountryCode } from 'src/constants/CountryCode';
 import { InputProps } from 'src/types/components/Input';
 import { OptionalKeys } from 'src/utils/type/OptionalKeys';
 
@@ -17,6 +18,7 @@ export type FormCurrencyNumberStringFieldProps = {
     required?: ValidationRule<boolean>;
     min?: ValidationRule<number>;
     max?: ValidationRule<number>;
+    country?: CountryCode;
     tooltip?: string;
     rules?: Record<string, ValidationRule<any>>;
     inputProps?: OptionalKeys<InputProps>;
