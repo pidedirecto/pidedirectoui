afc60e56ee1aadfff8dc605db9cee65e3cc2dc00
Oscar Moreno <oscar@pidedirecto.mx>
2024-07-01 12:49:14 -0600

    improve loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,22 +1,46 @@
 /**
  * @prettier
  */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Text } from 'src/components/Text';
 import classes from 'src/styles/loader.module.css';
 import { LoaderProps } from 'src/types/components/Loader';
 
-export function Loader({ loading, text }: LoaderProps): React.ReactElement | null {
+export function Loader({ loading, text, size }: LoaderProps): React.ReactElement | null {
+    const rootElement = useRef<HTMLElement | null>(null);
+
+    const containerHeight = Math.floor((size ?? DefaultSize) * 1.5);
+
+    useEffect(() => {
+        initializeSize();
+    }, [size]);
+
+    const initializeSize = () => {
+        if (!size) return;
+
+        if (!rootElement.current) {
+            rootElement.current = document.querySelector(':root') as any;
+        }
+        if (!rootElement.current) return;
+
+        const newLoaderPosition = Math.floor((size * DefaultPosition) / DefaultSize);
+        rootElement.current.style.setProperty('--loader-initial-animation-position', `-${newLoaderPosition}px`);
+        rootElement.current.style.setProperty('--loader-final-animation-position', `${newLoaderPosition}px`);
+    };
+
     if (!loading) return null;
 
     return (
         <div className={classes.loaderContainer}>
-            <div className={classes.slidingDots}>
-                <span></span>
-                <span></span>
-                <span></span>
+            <div className={classes.slidingDots} style={{ height: containerHeight }}>
+                <span style={{ width: size, height: size }}></span>
+                <span style={{ width: size, height: size }}></span>
+                <span style={{ width: size, height: size }}></span>
             </div>
-            <Text className={classes.textContainer}>{text}</Text>
+            {!!text && <Text className={classes.textContainer}>{text}</Text>}
         </div>
     );
 }
+
+const DefaultPosition = 40;
+const DefaultSize = 18;
--- src/types/components/Loader.d.ts
+++ src/types/components/Loader.d.ts
@@ -2,7 +2,8 @@ import * as React from "react";
 
 export type LoaderProps = {
     loading: boolean;
-    text?:string
+    text?: string
+    size?: number
 }
 
 export type Loader = React.FunctionComponent<LoaderProps>
