0599a624d0f26af91f0998f35f05d407a6cd31a1
Oscar Moreno <oscar@pidedirecto.mx>
2024-07-22 15:08:13 -0600

    add ImageUpload + utils + services

---
+++ src/utils/crypto/hashMd5FromArrayBuffer.ts
@@ -0,0 +1,11 @@
+/**
+ * @prettier
+ */
+import CryptoJS from 'crypto-js';
+
+export function hashMd5FromArrayBuffer(value?: string | ArrayBuffer | null): string {
+    if (!value) {
+        throw Error(`Cannot create hash md5 from value=${value}`);
+    }
+    return CryptoJS.MD5(CryptoJS.lib.WordArray.create(value as any)).toString();
+}
