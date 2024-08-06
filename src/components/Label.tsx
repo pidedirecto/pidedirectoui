e527c5976906745708ce0c5055c4606b68ebc4ce
Oscar Moreno <oscar@pidedirecto.mx>
2024-08-06 12:05:47 -0600

    add variant prop to Label

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -6,7 +6,15 @@ import { HTMLProps } from 'react';
 import classes from 'src/styles/label.module.css';
 import { classNames } from 'src/utils/css/classNames';
 
-export function Label({ children, error, classes: classesProp, ...props }: Props): React.ReactElement {
+export function Label({ children, error, variant, classes: classesProp, ...props }: Props): React.ReactElement {
+    if (variant === 'legend') {
+        return (
+            <legend {...props} className={classNames(classes.label, classesProp?.label, error && classNames(classes.labelError, classesProp?.error))}>
+                {children}
+            </legend>
+        );
+    }
+
     return (
         <label {...props} className={classNames(classes.label, classesProp?.label, error && classNames(classes.labelError, classesProp?.error))}>
             {children}
@@ -14,11 +22,13 @@ export function Label({ children, error, classes: classesProp, ...props }: Props
     );
 }
 
-type Props = Omit<HTMLProps<HTMLLabelElement>, 'className'> & {
-    children: React.ReactNode;
-    classes?: {
-        label?: string;
-        error?: string;
+type Props = Omit<HTMLProps<HTMLLabelElement>, 'className'> &
+    Omit<HTMLProps<HTMLLegendElement>, 'className'> & {
+        children: React.ReactNode;
+        variant?: 'legend';
+        classes?: {
+            label?: string;
+            error?: string;
+        };
+        error?: boolean;
     };
-    error?: boolean;
-};
