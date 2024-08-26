234685a87f59fe71cb9255bb508bc31e99b38444
Hector Wong <hector.wong@pidedirecto.mx>
2024-08-26 08:19:34 -0600

    add country code for latam

--- src/constants/CountryCode.ts
+++ src/constants/CountryCode.ts
@@ -13,6 +13,16 @@ export const CountryCodes = {
     CL: 'CL',
     PY: 'PY',
     CO: 'CO',
+    BO: 'BO',
+    CR: 'CR',
+    CU: 'CU',
+    EC: 'EC',
+    SV: 'SV',
+    GT: 'GT',
+    NI: 'NI',
+    PA: 'PA',
+    DO: 'DO',
+    HN: 'HN',
 } as const;
 
 export type CountryCode = (typeof CountryCodes)[keyof typeof CountryCodes];
