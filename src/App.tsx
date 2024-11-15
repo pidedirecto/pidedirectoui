0fd04da4e6cd5e76ef88ce24fb2b1770b3e79369
Oscar Moreno <oscar@pidedirecto.mx>
2024-11-15 17:44:48 -0600

    update createStore to support indexed db

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Button } from 'src/components/Button';
-import { createIndexedStore } from 'src/hooks/createIndexedStore';
+import { createStore } from 'src/hooks/createStore';
 
 export function App(): React.ReactElement {
     const columns = [
@@ -39,110 +38,105 @@ export function App(): React.ReactElement {
     ];
     return (
         <div style={{ padding: 120 }}>
-            <SelectedOrder />
-            <Orders />
-            <Number />
-            <Actions />
+            <RegularNumber />
+            <IndexedNumber />
         </div>
     );
 }
 
-function Actions() {
-    const updateOrders = useActions((actions) => actions.updateOrders);
-    const updateNumber = useActions((actions) => actions.updateNumber);
-    const addOrders = useActions((actions) => actions.addOrders);
+function RegularNumber() {
+    const number = useRegularStore((state) => state.number);
 
-    console.log('rendering actions');
-
-    const handleAddOrders = async () => {
-        await addOrders({
-            orderId: Math.random().toString(),
-            name: 'some name',
-        });
-    };
-
-    const handleUpdateOrder = async () => {
-        await updateOrders({
-            orderId: '0.1857904290897454',
-            name: 'update name' + new Date().toISOString(),
-        });
-    };
-
-    const handleUpdateNumber = async () => {
-        await updateNumber(Math.random());
-    };
+    const updateNumber = useRegularActions((actions) => actions.updateNumber);
 
     return (
         <div>
-            <Button onClick={handleAddOrders}>Add order</Button>
-            <Button onClick={handleUpdateOrder}>Update order</Button>
-            <Button onClick={handleUpdateNumber}>Update number</Button>
+            <h1>Number is {number}</h1>
+            <button onClick={() => updateNumber(Math.random())}>Update number</button>
         </div>
     );
 }
 
-function Orders() {
-    const orders = useStore((state) => state.orders);
+function IndexedNumber() {
+    const number = usePersistStore((state) => state.number);
+    const otherNumber = useOtherPersistStore((state) => state.number);
 
-    const removeOrders = useActions((actions) => actions.removeOrders);
-    const updateSelectedOrderId = useActions((actions) => actions.updateSelectedOrderId);
+    console.log('number = ', number);
 
-    console.log('rendering orders');
+    const updateNumber = usePersistActions((actions) => actions.updateNumber);
+    const updateOtherNumber = useOtherPersistActions((actions) => actions.updateNumber);
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-            {orders.map((order) => (
-                <div key={order.orderId}>
-                    <span>{order.name}</span>
-                    <button onClick={() => removeOrders(order.orderId)}>remove</button>
-                    <button onClick={() => updateSelectedOrderId(order.orderId)}>select</button>
-                </div>
-            ))}
+        <div>
+            <h1>Indexed number is {number}</h1>
+            <h1>Second indexed number is {otherNumber}</h1>
+            <button
+                onClick={async () => {
+                    await updateNumber(Math.random());
+                    await updateOtherNumber(Math.random());
+                }}
+            >
+                Update number
+            </button>
         </div>
     );
 }
 
-function Number() {
-    const number = useStore((state) => state.number);
-    console.log('rendering number');
-
-    return <span>{number}</span>;
-}
-
-function SelectedOrder() {
-    const selectedOrderId = useStore((state) => state.selectedOrderId);
-    console.log('rendering SelectedOrder');
-
-    return <h1>SELECTED ORDER IS {selectedOrderId}</h1>;
-}
-
-const [useStore, useActions] = createIndexedStore<State>({
-    name: 'myDB',
-    version: 2,
-    schema: {
-        orders: {
-            type: 'array',
-            key: 'orderId',
-        },
-        number: {
-            type: 'primitive',
-        },
-        selectedOrderId: {
-            type: 'primitive',
-        },
-    },
+const [useRegularStore, useRegularActions] = createStore<RegularState, RegularActions>({
     initialState: {
-        orders: [],
-        number: 0,
-        selectedOrderId: undefined,
+        number: 1,
+        string: 'Some string',
+    },
+    actions: {
+        updateNumber(state, number) {
+            state.number = number;
+        },
+        updateString(state, string) {
+            state.string = string;
+        },
     },
 });
 
-type State = {
-    orders: Array<{
-        orderId: string;
-        name: string;
-    }>;
+const [usePersistStore, usePersistActions] = createStore<RegularState, RegularActions>({
+    initialState: {
+        number: 1,
+        string: 'Some string',
+    },
+    actions: {
+        updateNumber(state, number) {
+            state.number = number;
+        },
+        updateString(state, string) {
+            state.string = string;
+        },
+    },
+    persist: true,
+    storeName: 'test',
+});
+
+const [useOtherPersistStore, useOtherPersistActions] = createStore<RegularState, RegularActions>({
+    initialState: {
+        number: 1,
+        string: 'Some string',
+    },
+    actions: {
+        updateNumber(state, number) {
+            state.number = number;
+        },
+        updateString(state, string) {
+            state.string = string;
+        },
+    },
+    persist: true,
+    storeName: 'otherTest',
+});
+
+type RegularState = {
     number: number;
-    selectedOrderId: string | undefined;
+    string: string;
+};
+
+type RegularActions = {
+    updateNumber: (number: number) => void;
+    updateString: (string: string) => void;
 };
--- src/hooks/createStore.ts
+++ src/hooks/createStore.ts
@@ -1,25 +1,60 @@
 import { enableMapSet, produce } from 'immer';
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
 import type { Params, Result } from 'src/types/hooks/CreateStore';
+import { createDb } from 'src/utils/indexedDb/createDb';
+import { getAllObjectStoreValues } from 'src/utils/indexedDb/getAllObjectStoreValues';
+import { getObjectStoreValue } from 'src/utils/indexedDb/getObjectStoreValue';
+import { populateInitialStateInIndexedDb } from 'src/utils/indexedDb/populateInitialStateInIndexedDb';
+import { IndexedSchema } from 'src/utils/indexedDb/type/IndexedSchema';
+import { updateValueFromObjectStore } from 'src/utils/indexedDb/updateValueFromObjectStore';
 import { cloneObject } from 'src/utils/object/cloneObject';
 import { isObject } from 'src/utils/object/isObject';
 
 enableMapSet();
 
-export function createStore<State, Actions>({ initialState, actions }: Params<State, Actions>): Result<State, Actions> {
+export function createStore<State extends Record<string, any>, Actions>(params: Params<State, Actions>): Result<State, Actions> {
+    const { initialState, actions, persist } = params;
     if (!isObject(initialState)) {
         console.error('initialState passed to createStore function is not an object');
     }
 
+    let db: IDBDatabase | undefined = undefined;
     let state: State = cloneObject(initialState as any);
     let listeners: Array<Listener> = [];
+
+    if (persist) initializeDb();
+
+    async function initializeDb() {
+        const indexedDB = window.indexedDB;
+        if (!indexedDB || !persist) return;
+
+        db = await createDb(`pidedirecto-store-${params.storeName}`, 1, persistStoreSchema);
+        await populateInitialStateInIndexedDb(db, initialState as State, persistStoreSchema);
+        const stateInIndexedDb = (await getDbState(db, persistStoreSchema)) as State;
+        state = (
+            stateInIndexedDb.store
+                ? {
+                      ...(stateInIndexedDb.store ?? cloneObject(initialState as any)),
+                  }
+                : cloneObject(initialState as any)
+        ) as State;
+        notifyChanges();
+    }
 
     function useStore<T>(fn: (state: State) => T): T {
         return useSyncExternalStore<T>(subscribeToStore, getSnapshotStore(fn));
     }
 
-    function useAction<T extends (...args: Array<any>) => void>(fn: (actions: Actions) => T): T {
+    function useAction<T extends (...args: Array<any>) => any>(fn: (actions: Actions) => T): T {
         const action = fn(actions as any);
+
+        if (persist) {
+            return (async (...args: Array<any>) => {
+                state = produce(state, (draft) => action(draft as any, ...args));
+                notifyChanges();
+                if (db) await updateValueFromObjectStore(db, 'store', state, 1);
+            }) as T;
+        }
 
         return ((...args: Array<any>) => {
             state = produce(state, (draft) => action(draft as any, ...args));
@@ -45,5 +80,35 @@ export function createStore<State, Actions>({ initialState, actions }: Params<St
 
     return [useStore, useAction];
 }
+
+async function getDbState<State extends Record<string, any>>(db: IDBDatabase, schema: Record<string, IndexedSchema>): Promise<Record<string, any>> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let state = {};
+            for (const [objectStoreName, objectStoreSchema] of Object.entries(schema)) {
+                if (objectStoreSchema.type === 'array') {
+                    state = {
+                        ...state,
+                        [objectStoreName]: await getAllObjectStoreValues(db, objectStoreName),
+                    };
+                    continue;
+                }
+                state = {
+                    ...state,
+                    [objectStoreName]: await getObjectStoreValue(db, objectStoreName, 1),
+                };
+            }
+            resolve(state);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+const persistStoreSchema: Record<string, IndexedSchema> = {
+    store: {
+        type: 'object',
+    },
+};
 
 type Listener = () => void;
