707929b613f3b895169a593b5af77034e918d418
Oscar Moreno <oscar@pidedirecto.mx>
2024-11-19 16:25:38 -0600

    add persist support to createStore

--- src/hooks/createStore.ts
+++ src/hooks/createStore.ts
@@ -2,14 +2,13 @@ import { enableMapSet, produce } from 'immer';
 import { useSyncExternalStore } from 'use-sync-external-store/shim';
 import type { Params, Result } from 'src/types/hooks/CreateStore';
 import { createDb } from 'src/utils/indexedDb/createDb';
-import { getAllObjectStoreValues } from 'src/utils/indexedDb/getAllObjectStoreValues';
-import { getObjectStoreValue } from 'src/utils/indexedDb/getObjectStoreValue';
+import { getStoreFromIndexedDb } from 'src/utils/indexedDb/getStoreFromIndexedDb';
 import { populateInitialStateInIndexedDb } from 'src/utils/indexedDb/populateInitialStateInIndexedDb';
 import { IndexedSchema } from 'src/utils/indexedDb/type/IndexedSchema';
-import { updateValueFromObjectStore } from 'src/utils/indexedDb/updateValueFromObjectStore';
+import { updateStoreInIndexedDb } from 'src/utils/indexedDb/updateStoreInIndexedDb';
 import { cloneObject } from 'src/utils/object/cloneObject';
 import { isObject } from 'src/utils/object/isObject';
-import { wait } from 'src/utils/wait';
+import { PersistPromise } from 'src/utils/promise/PersistPromise';
 
 enableMapSet();
 
@@ -30,16 +29,9 @@ export function createStore<State extends Record<string, any>, Actions>(params: 
         const indexedDB = window.indexedDB;
         if (!indexedDB || !persist) return;
 
-        db = await createDb(`pidedirecto-store-${params.storeName}`, 1, persistStoreSchema);
-        await populateInitialStateInIndexedDb(db, initialState as State, persistStoreSchema);
-        const stateInIndexedDb = (await getDbState(db, persistStoreSchema)) as State;
-        state = (
-            stateInIndexedDb.store
-                ? {
-                      ...(stateInIndexedDb.store ?? cloneObject(initialState as any)),
-                  }
-                : cloneObject(initialState as any)
-        ) as State;
+        db = await createDb('pidedirecto', 1, persistStoreSchema);
+        await populateInitialStateInIndexedDb(db, params.storeName, initialState);
+        state = ((await getStoreFromIndexedDb(db, params.storeName)) ?? cloneObject(initialState as any)) as State;
         notifyChanges();
     }
 
@@ -57,7 +49,7 @@ export function createStore<State extends Record<string, any>, Actions>(params: 
                 if (db) {
                     const previousPersistPromise = persistPromise;
                     previousPersistPromise.cancel();
-                    persistPromise = new PersistPromise(() => previousPersistPromise.promise.then(() => updateValueFromObjectStore(db!, 'store', state, 1)));
+                    persistPromise = new PersistPromise(() => previousPersistPromise.promise.then(() => updateStoreInIndexedDb(db!, params.storeName, state)));
                 }
             }) as T;
         }
@@ -87,51 +79,11 @@ export function createStore<State extends Record<string, any>, Actions>(params: 
     return [useStore, useAction];
 }
 
-async function getDbState<State extends Record<string, any>>(db: IDBDatabase, schema: Record<string, IndexedSchema>): Promise<Record<string, any>> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let state = {};
-            for (const [objectStoreName, objectStoreSchema] of Object.entries(schema)) {
-                if (objectStoreSchema.type === 'array') {
-                    state = {
-                        ...state,
-                        [objectStoreName]: await getAllObjectStoreValues(db, objectStoreName),
-                    };
-                    continue;
-                }
-                state = {
-                    ...state,
-                    [objectStoreName]: await getObjectStoreValue(db, objectStoreName, 1),
-                };
-            }
-            resolve(state);
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
-
 const persistStoreSchema: Record<string, IndexedSchema> = {
     store: {
-        type: 'object',
+        type: 'array',
+        key: '__storeName',
     },
 };
 
 type Listener = () => void;
-
-class PersistPromise {
-    private cancelled = false;
-    public readonly promise: Promise<void>;
-
-    constructor(asyncFunction: () => Promise<void> = () => Promise.resolve()) {
-        this.promise = (async () => {
-            await wait(0); // needed to wait for cancelled to be set, without this updateValueFromObjectStore will be called every time an action is used
-            if (this.cancelled) return;
-            await asyncFunction();
-        })();
-    }
-
-    cancel(): void {
-        this.cancelled = true;
-    }
-}
